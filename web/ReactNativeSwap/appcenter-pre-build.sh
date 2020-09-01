adaed9a8bb1a94f8befbdbf8e0ce0fcbe0f8569f
aelf-hz <mason.huang@aelf.io>
2020-09-01 21:19:53 +0800

    test: 💍 fix

--- web/ReactNativeSwap/appcenter-pre-build.sh
+++ web/ReactNativeSwap/appcenter-pre-build.sh
@@ -1,9 +1,3 @@
-#!/usr/bin/env bash
-cd ~/.cocoapods/repos 
-pod repo remove master
-git clone https://mirrors.tuna.tsinghua.edu.cn/git/CocoaPods/Specs.git master
-
-cd -
 cd ios  
 pod install --repo-update
 cd ..
