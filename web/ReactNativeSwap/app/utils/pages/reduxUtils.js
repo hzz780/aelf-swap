8039bf6f06b237e5039232b48e6dda37a8f250e1
aelf-hz <mason.huang@aelf.io>
2020-09-08 18:13:00 +0800

    feat: 🎸 getTokenDecimals

--- web/ReactNativeSwap/app/utils/pages/reduxUtils.js
+++ web/ReactNativeSwap/app/utils/pages/reduxUtils.js
@@ -3,13 +3,29 @@ import unitConverter from './unitConverter';
 const getState = () => {
   return store.getState();
 };
+const getTokenDecimals = symbol => {
+  let decimals = 8;
+  if (store) {
+    const {user} = store.getState() || {};
+    const {allTokens} = user || {};
+    if (Array.isArray(allTokens)) {
+      const tokenO = allTokens.find(i => {
+        return i.symbol === symbol;
+      });
+      if (tokenO) {
+        decimals = tokenO.decimals || decimals;
+      }
+    }
+  }
+  return decimals;
+};
 const getDecimalTokenHigher = (token, symbol) => {
   let a = 0;
   if (store) {
     const {user} = store.getState() || {};
     const {tokenUSD} = user || {};
     if (tokenUSD) {
-      const {decimals} = tokenUSD[symbol] || {};
+      const decimals = getTokenDecimals(symbol);
       if (decimals) {
         a = unitConverter.toDecimalHigher(token, decimals);
       }
@@ -42,4 +58,5 @@ export default {
   getState,
   getDecimalTokenHigher,
   getAddData,
+  getTokenDecimals,
 };
